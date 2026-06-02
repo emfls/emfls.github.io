8d1e06b3bb7c1d6a820a696bc4f537adffe9e457
White Smile <whitesmile@Whiteui-MacBookAir.local>
2026-06-02 15:25:49 +0900

    robots.txt Disallow 제거, astro config 도메인 수정

--- kor/stockwiki/astro.config.mjs
+++ kor/stockwiki/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://emfls.github.io',
+  site: 'https://emfls.com',
   base: '/kor/stockwiki',
   output: 'static',
   integrations: [],
